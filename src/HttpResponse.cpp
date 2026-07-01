715bdd8358d9c19d48ad0835bb739120bf17d412
AimonKied <simon.wied2000@gmail.com>
2026-07-01 17:20:25 +0200

    FEAT(serveFile): Added serveFile method with check for existing file

--- src/HttpResponse.cpp
+++ src/HttpResponse.cpp
@@ -14,3 +14,23 @@ std::string Response::toString() const {
     result += "\r\n" + body;
     return result;
 };
+
+    Response Response::serveFile(const std::string& filePath) {
+        Response res;
+        std::ifstream file(filePath);
+
+        if (!file.is_open()) {
+            res.statusCode = 404;
+            res.statusText = "Not Found";
+            res.body = "404 Not Found";
+            res.headers["Content-Length"] = std::to_string(res.body.size());
+            return res;
+        }
+        std::stringstream ss;
+        ss << file.rdbuf();
+        res.body = ss.str();
+        res.statusCode = 200;
+        res.statusText = "OK";
+        res.headers["Content-Length"] = std::to_string(res.body.size());
+        return res;
+};
