4c531b84846ab5ac2ad9347397008e0310d9eb05
AimonKied <simon.wied2000@gmail.com>
2026-07-01 16:30:23 +0200

    FEAT(class): Added HttpRequest.hpp with basic Request class + enum class Method

---
+++ include/HttpRequest.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+
+enum class Method {GET, POST, DELETE, UNKNOWN};
+
+class Request {
+    public:
+        Method  method;
+        std::string uri;
+        std::string path;
+        std::string query;
+        std::string version;
+
+        std::unordered_map<std::string, std::string> headers;
+
+        std::string body;
+
+        bool complete = false;
+        int errorCode = 0;
+};
