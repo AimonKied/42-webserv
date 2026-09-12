37f01841b6ecbb52f7783af866d3129d89090192
Aimon <simon.wied2000@gmail.com>
2026-09-12 23:02:56 +0200

    FEAT(AutoIndex): Add escapeHtml to buildDirectoryListing so filenames can have special characters without interupting the html body cause the chars get changed into Entities

--- src/AutoIndex.cpp
+++ src/AutoIndex.cpp
@@ -3,6 +3,29 @@
 
 Response makeErrorResponse(int code, const LocationConfig& loc);
 
+/*
+HTML-Escaping: Sonderzeichen in HTML-Entities umwandeln, damit sie nicht als HTML interpretiert werden.
+&amp; -> &
+&lt; -> <
+&gt; -> >
+&quot; -> "
+&#39; -> '
+*/
+static std::string escapeHtml(const std::string& str) {
+    std::string escaped;
+    for (char c : str) {
+        switch (c) {
+            case '&': escaped += "&amp;"; break;
+            case '<': escaped += "&lt;"; break;
+            case '>': escaped += "&gt;"; break;
+            case '"': escaped += "&quot;"; break;
+            case '\'': escaped += "&#39;"; break;
+            default: escaped += c; break;
+        }
+    }
+    return escaped;
+}
+
 Response buildDirectoryListing(const std::string& fullPath, const std::string& urlPath, const LocationConfig& loc) {
     Response res;
 
@@ -20,9 +43,9 @@ Response buildDirectoryListing(const std::string& fullPath, const std::string& u
     res.body = "<html><head><title>Index of " + urlPath + "</title></head><body><h1>Index of " + urlPath + "</h1><ul>";
     for (const auto& entry : it) {
         if (entry.is_directory()) {
-            res.body += "<li><a href=\"" + urlPath + entry.path().filename().string() + "/\">" + entry.path().filename().string() + "/</a></li>";
+            res.body += "<li><a href=\"" + urlPath + entry.path().filename().string() + "/\">" + escapeHtml(entry.path().filename().string()) + "/</a></li>";
         } else {
-            res.body += "<li><a href=\"" + urlPath + entry.path().filename().string() + "\">" + entry.path().filename().string() + "</a></li>";
+            res.body += "<li><a href=\"" + urlPath + entry.path().filename().string() + "\">" + escapeHtml(entry.path().filename().string()) + "</a></li>";
         }
     }
     res.body += "</ul></body></html>";
